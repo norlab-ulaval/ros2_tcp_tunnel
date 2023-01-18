93535a5c80910548067400cc83f792fbd37c99d6
simonpierredeschenes <simon-pierre.deschenes.1@ulaval.ca>
2023-01-18 00:14:47 -0500

    First working version on ROS 1.

--- src/client.cpp
+++ src/client.cpp
@@ -1,179 +1,197 @@
-#include <rclcpp/rclcpp.hpp>
+#include <ros/ros.h>
 #include <netinet/in.h>
 #include <fcntl.h>
-#include <tcp_tunnel/srv/add_topic.hpp>
-#include <tcp_tunnel/srv/register_client.hpp>
+#include <tcp_tunnel/AddTopic.h>
+#include <tcp_tunnel/RegisterClient.h>
+#include <thread>
+#include <topic_tools/shape_shifter.h>
+#include <rosmsg_cpp/rosmsg_cpp.h>
 
-class TCPTunnelClient : public rclcpp::Node
+std::unique_ptr<ros::NodeHandle> nodeHandle;
+std::string clientIp;
+ros::ServiceClient registerClientClient;
+std::vector<ros::Publisher> publishers;
+std::vector<int> listeningSockets;
+std::vector<int> connectedSockets;
+std::vector<std::thread> threads;
+
+void readFromSocket(const int& socketfd, void* buffer, const size_t& nbBytesToRead)
 {
-public:
-    TCPTunnelClient():
-            Node("tcp_tunnel_client")
-    {
-        this->declare_parameter<std::string>("client_ip", "127.0.0.1");
-        this->get_parameter("client_ip", clientIp);
+	size_t nbBytesRead = 0;
+	while(ros::ok() && nbBytesRead < nbBytesToRead)
+	{
+		int n = read(socketfd, ((char*)buffer) + nbBytesRead, nbBytesToRead - nbBytesRead);
+		if(n >= 0)
+		{
+			nbBytesRead += n;
+		}
+	}
+	if(nbBytesRead < nbBytesToRead)
+	{
+		throw std::runtime_error("An error occurred while reading from socket.");
+	}
+}
 
-        registerClientClient = this->create_client<tcp_tunnel::srv::RegisterClient>("/tcp_tunnel_server/register_client");
-        addTopicService = this->create_service<tcp_tunnel::srv::AddTopic>("/tcp_tunnel_client/add_topic",
-                                                                          std::bind(&TCPTunnelClient::addTopicCallback, this, std::placeholders::_1));
-    }
+void publishMessageLoop(int threadId)
+{
+	size_t size;
+	while(ros::ok())
+	{
+		int n = read(connectedSockets[threadId], &size, sizeof(size_t));
+		if(n > 0)
+		{
+			readFromSocket(connectedSockets[threadId], ((char*)&size) + n, sizeof(size_t) - n);
+			std::vector<uint8_t> buffer;
+			buffer.resize(size);
+			readFromSocket(connectedSockets[threadId], buffer.data(), size);
+			ros::serialization::IStream stream(buffer.data(), buffer.size());
+			topic_tools::ShapeShifter msg;
+			msg.read(stream);
+			publishers[threadId].publish(msg);
+		}
+		else if(n == 0)
+		{
+			throw std::runtime_error("Connection closed by server.");
+		}
+		else if(errno == EWOULDBLOCK)
+		{
+			std::this_thread::sleep_for(std::chrono::milliseconds(10));
+		}
+		else
+		{
+			throw std::runtime_error("An error occurred while reading from socket.");
+		}
+	}
+}
 
-    ~TCPTunnelClient()
-    {
-        for(size_t i = 0; i < threads.size(); ++i)
-        {
-            close(listeningSockets[i]);
-            close(connectedSockets[i]);
-            threads[i].join();
-        }
-    }
+void registerAsClient(const std::string& topicName, const struct sockaddr_in& serv_addr)
+{
+	tcp_tunnel::RegisterClient clientRequest;
+	clientRequest.request.topic.data = topicName;
+	std_msgs::String ip;
+	ip.data = clientIp;
+	clientRequest.request.client_ip = ip;
+	std_msgs::UInt16 port;
+	port.data = ntohs(serv_addr.sin_port);
+	clientRequest.request.client_port = port;
+	registerClientClient.call(clientRequest);
+}
 
-    void addTopicCallback(const std::shared_ptr<tcp_tunnel::srv::AddTopic::Request> req)
-    {
-        std::string topicName = req->topic.data;
-        if(this->get_topic_names_and_types().count(topicName) == 0)
-        {
-            RCLCPP_ERROR_STREAM(this->get_logger(), "No topic named " << topicName);
-            return;
-        }
-        std::string topicType = this->get_topic_names_and_types()[topicName][0];
-
-        // create socket
-        int sockfd;
-        struct sockaddr_in serv_addr;
-
-        sockfd = socket(AF_INET, SOCK_STREAM, 0);
-        if(sockfd < 0)
-        {
-            RCLCPP_ERROR_STREAM(this->get_logger(), "Error \"" << strerror(errno) << "\" occurred while creating a socket connection for topic " << topicName << ".");
-            return;
-        }
-        fcntl(sockfd, F_SETFL, O_NONBLOCK);
-
-        bzero(&serv_addr, sizeof(serv_addr));
-        serv_addr.sin_family = AF_INET;
-        serv_addr.sin_addr.s_addr = INADDR_ANY;
-        serv_addr.sin_port = htons(0);
-        if(bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
-        {
-            RCLCPP_ERROR_STREAM(this->get_logger(), "Error \"" << strerror(errno) << "\" occurred while trying to bind to socket for topic " << topicName << ".");
-            return;
-        }
-        listeningSockets.push_back(sockfd);
-
-        socklen_t socklen = sizeof(serv_addr);
-        bzero(&serv_addr, socklen);
-        if(getsockname(sockfd, (struct sockaddr*)&serv_addr, &socklen) < 0)
-        {
-            RCLCPP_ERROR_STREAM(this->get_logger(), "Error \"" << strerror(errno) << "\" occurred while trying to retrieve TCP port assigned for topic " << topicName << ".");
-            return;
-        }
-
-        // call register_client service
-        std::shared_ptr<tcp_tunnel::srv::RegisterClient::Request> clientRequest = std::make_shared<tcp_tunnel::srv::RegisterClient::Request>();
-        clientRequest->topic = req->topic;
-        std_msgs::msg::String ip;
-        ip.data = clientIp;
-        clientRequest->client_ip = ip;
-        std_msgs::msg::UInt16 port;
-        port.data = ntohs(serv_addr.sin_port);
-        clientRequest->client_port = port;
-        registerClientClient->async_send_request(clientRequest);
-
-        // connect to server
-        int newsockfd = -1;
-        struct sockaddr_in cli_addr;
-        socklen_t clilen;
-
-        listen(sockfd, 5);
-        clilen = sizeof(cli_addr);
-        std::chrono::time_point<std::chrono::steady_clock> startTime = std::chrono::steady_clock::now();
-        while(std::chrono::steady_clock::now() - startTime < std::chrono::duration<float>(3) && newsockfd < 0)
-        {
-            newsockfd = accept(sockfd, (struct sockaddr*)&cli_addr, &clilen);
-        }
-        if(newsockfd < 0)
-        {
-            RCLCPP_ERROR_STREAM(this->get_logger(), "Error \"" << strerror(errno) << "\" occurred while accepting connection for topic " << topicName << ".");
-            return;
-        }
-        fcntl(newsockfd, F_SETFL, O_NONBLOCK);
-        connectedSockets.push_back(newsockfd);
-
-        // create publisher
-        rclcpp::QoS qos = rclcpp::QoS(rclcpp::KeepLast(10));
-        std::string prefix = "/tcp_tunnel_client";
-        if(topicName[0] != '/')
-        {
-            prefix += "/";
-        }
-        publishers.push_back(this->create_generic_publisher(prefix + topicName, topicType, qos));
-
-        // create thread
-        threads.emplace_back(&TCPTunnelClient::publishMessageLoop, this, threads.size());
-
-        RCLCPP_INFO_STREAM(this->get_logger(), "Successfully added topic to TCP tunnel, new topic " << prefix + topicName << " has been created.");
-    }
-
-    void publishMessageLoop(int threadId)
-    {
-        rclcpp::SerializedMessage msg;
-        while(rclcpp::ok())
-        {
-            int n = read(connectedSockets[threadId], &msg.get_rcl_serialized_message().buffer_length, sizeof(size_t));
-            if(n > 0)
-            {
-                readFromSocket(connectedSockets[threadId], ((char*)&msg.get_rcl_serialized_message().buffer_length) + n, sizeof(size_t) - n);
-                msg.reserve(msg.get_rcl_serialized_message().buffer_length);
-                readFromSocket(connectedSockets[threadId], msg.get_rcl_serialized_message().buffer, msg.get_rcl_serialized_message().buffer_length);
-                publishers[threadId]->publish(msg);
-            }
-            else if(n == 0)
-            {
-                throw std::runtime_error("Connection closed by server.");
-            }
-            else if(errno == EWOULDBLOCK)
-            {
-                std::this_thread::sleep_for(std::chrono::milliseconds(10));
-            }
-            else
-            {
-                throw std::runtime_error("An error occurred while reading from socket.");
-            }
-        }
-    }
-
-    void readFromSocket(const int& socketfd, void* buffer, const size_t& nbBytesToRead)
-    {
-        size_t nbBytesRead = 0;
-        while(rclcpp::ok() && nbBytesRead < nbBytesToRead)
-        {
-            int n = read(socketfd, ((char*)buffer) + nbBytesRead, nbBytesToRead - nbBytesRead);
-            if(n >= 0)
-            {
-                nbBytesRead += n;
-            }
-        }
-        if(nbBytesRead < nbBytesToRead)
-        {
-            throw std::runtime_error("An error occurred while reading from socket.");
-        }
-    }
-
-private:
-    rclcpp::Client<tcp_tunnel::srv::RegisterClient>::SharedPtr registerClientClient;
-    rclcpp::Service<tcp_tunnel::srv::AddTopic>::SharedPtr addTopicService;
-    std::vector<rclcpp::GenericPublisher::SharedPtr> publishers;
-    std::vector<int> listeningSockets;
-    std::vector<int> connectedSockets;
-    std::vector<std::thread> threads;
-    std::string clientIp;
-};
+bool addTopicCallback(tcp_tunnel::AddTopic::Request& req, tcp_tunnel::AddTopic::Response& res)
+{
+	ros::master::V_TopicInfo advertisedTopics;
+	ros::master::getTopics(advertisedTopics);
+	
+	std::string topicName = req.topic.data;
+	bool topicIsAdvertised = false;
+	std::string topicType;
+	for(size_t i = 0; i < advertisedTopics.size(); ++i)
+	{
+		if(advertisedTopics[i].name == topicName)
+		{
+			topicIsAdvertised = true;
+			topicType = advertisedTopics[i].datatype;
+			break;
+		}
+	}
+	if(!topicIsAdvertised)
+	{
+		ROS_ERROR_STREAM("No topic named " << topicName);
+		return false;
+	}
+	
+	// create socket
+	int sockfd;
+	struct sockaddr_in serv_addr;
+	
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(sockfd < 0)
+	{
+		ROS_ERROR_STREAM("Error \"" << strerror(errno) << "\" occurred while creating a socket connection for topic " << topicName << ".");
+		return false;
+	}
+	fcntl(sockfd, F_SETFL, O_NONBLOCK);
+	
+	bzero(&serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(0);
+	if(bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
+	{
+		ROS_ERROR_STREAM("Error \"" << strerror(errno) << "\" occurred while trying to bind to socket for topic " << topicName << ".");
+		return false;
+	}
+	listeningSockets.push_back(sockfd);
+	
+	socklen_t socklen = sizeof(serv_addr);
+	bzero(&serv_addr, socklen);
+	if(getsockname(sockfd, (struct sockaddr*)&serv_addr, &socklen) < 0)
+	{
+		ROS_ERROR_STREAM("Error \"" << strerror(errno) << "\" occurred while trying to retrieve TCP port assigned for topic " << topicName << ".");
+		return false;
+	}
+	
+	// call register_client service
+	std::thread registerClientThread(registerAsClient, topicName, serv_addr);
+	
+	// connect to server
+	int newsockfd = -1;
+	struct sockaddr_in cli_addr;
+	socklen_t clilen;
+	
+	listen(sockfd, 5);
+	clilen = sizeof(cli_addr);
+	std::chrono::time_point<std::chrono::steady_clock> startTime = std::chrono::steady_clock::now();
+	while(std::chrono::steady_clock::now() - startTime < std::chrono::duration<float>(3) && newsockfd < 0)
+	{
+		newsockfd = accept(sockfd, (struct sockaddr*)&cli_addr, &clilen);
+	}
+	if(newsockfd < 0)
+	{
+		ROS_ERROR_STREAM("Error \"" << strerror(errno) << "\" occurred while accepting connection for topic " << topicName << ".");
+		return false;
+	}
+	fcntl(newsockfd, F_SETFL, O_NONBLOCK);
+	connectedSockets.push_back(newsockfd);
+	
+	registerClientThread.join();
+	
+	// create publisher
+	std::string prefix = "/tcp_tunnel_client";
+	if(topicName[0] != '/')
+	{
+		prefix += "/";
+	}
+	topic_tools::ShapeShifter shapeShifter;
+	shapeShifter.morph(ros::message::getMD5Sum(topicType), topicType, ros::message::getFullDef(topicType), "");
+	publishers.push_back(shapeShifter.advertise(*nodeHandle, prefix + topicName, 1));
+	
+	// create thread
+	threads.emplace_back(publishMessageLoop, threads.size());
+	
+	ROS_INFO_STREAM("Successfully added topic to TCP tunnel, new topic " << prefix + topicName << " has been created.");
+	return true;
+}
 
 int main(int argc, char** argv)
 {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<TCPTunnelClient>());
-    rclcpp::shutdown();
-    return 0;
+	ros::init(argc, argv, "tcp_tunnel_client");
+	nodeHandle = std::make_unique<ros::NodeHandle>();
+	ros::NodeHandle privateNodeHandle("~");
+	
+	privateNodeHandle.param<std::string>("client_ip", clientIp, "127.0.0.1");
+	
+	registerClientClient = nodeHandle->serviceClient<tcp_tunnel::RegisterClient>("/tcp_tunnel_server/register_client");
+	ros::ServiceServer addTopicService = nodeHandle->advertiseService("/tcp_tunnel_client/add_topic", addTopicCallback);
+	
+	ros::spin();
+	
+	for(size_t i = 0; i < threads.size(); ++i)
+	{
+		close(listeningSockets[i]);
+		close(connectedSockets[i]);
+		threads[i].join();
+	}
+	
+	return 0;
 }
--- src/server.cpp
+++ src/server.cpp
@@ -1,130 +1,124 @@
-#include <rclcpp/rclcpp.hpp>
-#include <tcp_tunnel/srv/register_client.hpp>
+#include <ros/ros.h>
+#include <ros_type_introspection/ros_introspection.hpp>
+#include <topic_tools/shape_shifter.h>
+#include <tcp_tunnel/RegisterClient.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <fcntl.h>
 #include <netinet/tcp.h>
+#include <chrono>
 
-class TCPTunnelServer : public rclcpp::Node
+std::vector<ros::Subscriber> subscribers;
+std::vector<int> sockets;
+std::unique_ptr<ros::NodeHandle> nodeHandle;
+RosIntrospection::Parser parser;
+
+void writeToSocket(const int& socketfd, const void* buffer, const size_t& nbBytesToWrite)
 {
-public:
-    TCPTunnelServer():
-            Node("tcp_tunnel_server")
-    {
+	size_t nbBytesWritten = 0;
+	while(ros::ok() && nbBytesWritten < nbBytesToWrite)
+	{
+		int n = write(socketfd, ((char*)buffer) + nbBytesWritten, nbBytesToWrite - nbBytesWritten);
+		if(n >= 0)
+		{
+			nbBytesWritten += n;
+		}
+	}
+	if(nbBytesWritten < nbBytesToWrite)
+	{
+		throw std::runtime_error("An error occurred while writing to socket.");
+	}
+}
 
-        registerClientService = this->create_service<tcp_tunnel::srv::RegisterClient>("/tcp_tunnel_server/register_client",
-                                                                                      std::bind(&TCPTunnelServer::registerClientCallback, this, std::placeholders::_1));
-    }
+void subscriberCallback(const topic_tools::ShapeShifter::ConstPtr& msg, const std::string& topic_name, const int& subscriptionId)
+{
+	const std::string& datatype = msg->getDataType();
+	const std::string& definition = msg->getMessageDefinition();
+	
+	parser.registerMessageDefinition(topic_name, RosIntrospection::ROSType(datatype), definition);
+	std::vector<uint8_t> buffer;
+	buffer.resize(msg->size());
+	ros::serialization::OStream stream(buffer.data(), buffer.size());
+	msg->write(stream);
+	size_t size = msg->size();
+	
+	int n = read(sockets[subscriptionId], nullptr, 1);
+	if(n != 0)
+	{
+		writeToSocket(sockets[subscriptionId], &size, sizeof(size_t));
+		writeToSocket(sockets[subscriptionId], buffer.data(), size);
+	}
+	else
+	{
+		throw std::runtime_error("Connection closed by client.");
+	}
+}
 
-    ~TCPTunnelServer()
-    {
-        for(size_t i = 0; i < sockets.size(); ++i)
-        {
-            close(sockets[i]);
-        }
-    }
-
-    void registerClientCallback(const std::shared_ptr<tcp_tunnel::srv::RegisterClient::Request> req)
-    {
-        std::string topicName = req->topic.data;
-        if(this->get_topic_names_and_types().count(topicName) == 0)
-        {
-            RCLCPP_ERROR_STREAM(this->get_logger(), "No topic named " << topicName);
-            return;
-        }
-        std::string topicType = this->get_topic_names_and_types()[topicName][0];
-
-        // create socket
-        int sockfd;
-        struct sockaddr_in serv_addr;
-
-        sockfd = socket(AF_INET, SOCK_STREAM, 0);
-        if(sockfd < 0)
-        {
-            RCLCPP_ERROR_STREAM(this->get_logger(), "Error \"" << strerror(errno) << "\" occurred while creating a socket for topic " << topicName << ".");
-            return;
-        }
-        fcntl(sockfd, F_SETFL, O_NONBLOCK);
-
-        int flag = 1;
-        if(setsockopt(sockfd, IPPROTO_TCP, TCP_NODELAY, &flag, sizeof(int)) < 0)
-        {
-            RCLCPP_ERROR_STREAM(this->get_logger(), "Error \"" << strerror(errno) << "\" occurred while setting socket options for topic " << topicName << ".");
-            return;
-        }
-
-        // connect to client
-        bzero(&serv_addr, sizeof(serv_addr));
-        serv_addr.sin_family = AF_INET;
-        serv_addr.sin_addr.s_addr = inet_addr(req->client_ip.data.c_str());
-        serv_addr.sin_port = htons(req->client_port.data);
-        int n = -1;
-        std::chrono::time_point<std::chrono::steady_clock> startTime = std::chrono::steady_clock::now();
-        while(std::chrono::steady_clock::now() - startTime < std::chrono::duration<float>(3) && n < 0)
-        {
-            n = connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
-        }
-        if(n < 0)
-        {
-            RCLCPP_ERROR_STREAM(this->get_logger(),
-                                "Error \"" << strerror(errno) << "\" occurred while trying to connect to " << req->client_ip.data << " on port " << req->client_port.data
-                                           << " for topic " << topicName << ".");
-            return;
-        }
-        sockets.push_back(sockfd);
-
-        // create subscription
-        rclcpp::QoS qos = rclcpp::QoS(rclcpp::KeepLast(1));
-        subscriptions.push_back(
-                this->create_generic_subscription(topicName, topicType, qos, std::bind(&TCPTunnelServer::subscriptionCallback, this, std::placeholders::_1, subscriptions.size())));
-
-        RCLCPP_INFO_STREAM(this->get_logger(), "Successfully registered client for topic " << topicName << ".");
-    }
-
-    void subscriptionCallback(std::shared_ptr<rclcpp::SerializedMessage> msg, const int& subscriptionId)
-    {
-        int n = read(sockets[subscriptionId], nullptr, 1);
-        if(n != 0)
-        {
-            writeToSocket(sockets[subscriptionId], &msg->get_rcl_serialized_message().buffer_length, sizeof(size_t));
-            writeToSocket(sockets[subscriptionId], msg->get_rcl_serialized_message().buffer, msg->get_rcl_serialized_message().buffer_length);
-        }
-        else
-        {
-            throw std::runtime_error("Connection closed by client.");
-        }
-    }
-
-    void writeToSocket(const int& socketfd, const void* buffer, const size_t& nbBytesToWrite)
-    {
-        size_t nbBytesWritten = 0;
-        while(rclcpp::ok() && nbBytesWritten < nbBytesToWrite)
-        {
-            int n = write(socketfd, ((char*)buffer) + nbBytesWritten, nbBytesToWrite - nbBytesWritten);
-            if(n >= 0)
-            {
-                nbBytesWritten += n;
-            }
-        }
-        if(nbBytesWritten < nbBytesToWrite)
-        {
-            throw std::runtime_error("An error occurred while writing to socket.");
-        }
-    }
-
-private:
-    rclcpp::Service<tcp_tunnel::srv::RegisterClient>::SharedPtr registerClientService;
-    std::vector<rclcpp::GenericSubscription::SharedPtr> subscriptions;
-    std::vector<int> sockets;
-};
+bool registerClientCallback(tcp_tunnel::RegisterClient::Request& req, tcp_tunnel::RegisterClient::Response& res)
+{
+	std::string topicName = req.topic.data;
+	
+	// create socket
+	int sockfd;
+	struct sockaddr_in serv_addr;
+	
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(sockfd < 0)
+	{
+		ROS_ERROR_STREAM("Error \"" << strerror(errno) << "\" occurred while creating a socket for topic " << topicName << ".");
+		return false;
+	}
+	fcntl(sockfd, F_SETFL, O_NONBLOCK);
+	
+	int flag = 1;
+	if(setsockopt(sockfd, IPPROTO_TCP, TCP_NODELAY, &flag, sizeof(int)) < 0)
+	{
+		ROS_ERROR_STREAM("Error \"" << strerror(errno) << "\" occurred while setting socket options for topic " << topicName << ".");
+		return false;
+	}
+	
+	// connect to client
+	bzero(&serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = inet_addr(req.client_ip.data.c_str());
+	serv_addr.sin_port = htons(req.client_port.data);
+	int n = -1;
+	std::chrono::time_point<std::chrono::steady_clock> startTime = std::chrono::steady_clock::now();
+	while(std::chrono::steady_clock::now() - startTime < std::chrono::duration<float>(3) && n < 0)
+	{
+		n = connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+	}
+	if(n < 0)
+	{
+		ROS_ERROR_STREAM("Error \"" << strerror(errno) << "\" occurred while trying to connect to " << req.client_ip.data << " on port " << req.client_port.data
+									<< " for topic " << topicName << ".");
+		return false;
+	}
+	sockets.push_back(sockfd);
+	
+	// create subscriber
+	subscribers.push_back(nodeHandle->subscribe<topic_tools::ShapeShifter>(topicName, 1, std::bind(subscriberCallback, std::placeholders::_1, topicName,
+																								   subscribers.size())));
+	
+	ROS_INFO_STREAM("Successfully registered client for topic " << topicName << ".");
+	
+	return true;
+}
 
 int main(int argc, char** argv)
 {
-    rclcpp::init(argc, argv);
-    std::shared_ptr<rclcpp::Node> node = std::make_shared<TCPTunnelServer>();
-    rclcpp::executors::MultiThreadedExecutor executor;
-    executor.add_node(node);
-    executor.spin();
-    rclcpp::shutdown();
-    return 0;
+	ros::init(argc, argv, "tcp_tunnel_server");
+	nodeHandle = std::make_unique<ros::NodeHandle>();
+	
+	ros::ServiceServer registerClientService = nodeHandle->advertiseService("/tcp_tunnel_server/register_client", registerClientCallback);
+	
+	ros::MultiThreadedSpinner spinner;
+	spinner.spin();
+	
+	for(size_t i = 0; i < sockets.size(); ++i)
+	{
+		close(sockets[i]);
+	}
+	
+	return 0;
 }
